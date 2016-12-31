0a9d986e9562eb03c6cfefd01300d6d73157909b
Jared <jarednutt@outlook.com>
2016-12-30 17:00:39 -0800

    added north theme

---
+++ site/web/app/themes/siteorigin-north/inc/contributors.php
@@ -0,0 +1,38 @@
+<?php
+return json_decode( '{
+	"85ce7a450a7ee4895f3bd823505e2e12": {
+		"name": "adiraomj",
+		"email": "85ce7a450a7ee4895f3bd823505e2e12",
+		"loc": 4140,
+		"score": 210.0008591947992,
+		"percent": 91.40088376412692
+	},
+	"0f7f79a15dafbc4c1b64dfdc79c6937d": {
+		"name": "adiraomj",
+		"email": "0f7f79a15dafbc4c1b64dfdc79c6937d",
+		"loc": 389,
+		"score": 14.118490674782436,
+		"percent": 6.144939263765916
+	},
+	"b7122312e4008f8f2c76911080bca01a": {
+		"name": "Andrew Misplon",
+		"email": "b7122312e4008f8f2c76911080bca01a",
+		"loc": 36,
+		"score": 3.4597085722154324,
+		"percent": 1.5058053680317876
+	},
+	"a885c7bd5442dd2acd8c3e42001332c6": {
+		"name": "Alex S",
+		"email": "a885c7bd5442dd2acd8c3e42001332c6",
+		"loc": 23,
+		"score": 2.1380448605511826,
+		"percent": 0.9305637630770571
+	},
+	"36639e6e074b731b093bde5a77305179": {
+		"name": "Braam Genis",
+		"email": "36639e6e074b731b093bde5a77305179",
+		"loc": 2,
+		"score": 0.04091494256995957,
+		"percent": 0.017807840998325865
+	}
+}', true );
